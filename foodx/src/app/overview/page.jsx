bff5b16680741b25f608451f524cb912285139fc
Guruprasath <guruprasath@jarvisml.com>
2023-07-01 20:45:29 +0530

    FoodX - Sidebar Setup

---
+++ foodx/src/app/overview/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+        <div className='text-2xl font-bold'>Dashboard Overview</div>
+    </div>
+  )
+}
+
+export default page
